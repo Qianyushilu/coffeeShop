a34c6adc44c2c9bed101d67ca80f5f36761a32ab
Zou Juncheng <z20041219@outlook.com>
2024-12-26 23:31:52 +0800

    2024-12-26_23:31

--- X2402邹君诚_若雪咖啡/X2402邹君诚_若雪咖啡/Program.cs
+++ X2402邹君诚_若雪咖啡/X2402邹君诚_若雪咖啡/Program.cs
@@ -5,39 +5,39 @@ using System.Text;
 
 namespace X2402邹君诚_若雪咖啡
 {
+    struct Product  //定义结构体，产品
+    {
+        public string Name;
+        public double Price;
+        public Product(string name, double price) //构造函数初始化商品名称和价格
+        {
+            Name = name;
+            Price = price;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
             //界面
-            Console.WriteLine("----------------------------------");
-            Console.WriteLine("-                                -");
-            Console.WriteLine("-        模拟自动售货机          -");
-            Console.WriteLine("-            若雪咖啡            -");
-            Console.WriteLine("-        NO1 2024.11.02          -");
-            Console.WriteLine("-                                -");
-            Console.WriteLine("----------------------------------");
+            displayWelcomeScreen();  //使用方法输出界面
 
-            Console.WriteLine("----------------------------------");
-            Console.WriteLine("-                                -");
-            Console.WriteLine("-            喜欢您来            -");
-            Console.WriteLine("-        1-美式咖啡     12.0元   -");
-            Console.WriteLine("-        2-拿铁咖啡     15.5元   -");
-            Console.WriteLine("-        3-香草拿铁     16.5元   -");
-            Console.WriteLine("-        4-抹茶拿铁     16.5元   -");
-            Console.WriteLine("-        5-意式浓缩     10.0元   -");
-            Console.WriteLine("-        6-退出点餐              -");
-            Console.WriteLine("-                                -");
-            Console.WriteLine("----------------------------------");
+            //功能实现
 
-           //功能实现
-
-          
-            int choise = 0;            //选择           
-            string[] Names = { "美式咖啡", "拿铁咖啡", "香草拿铁", "抹茶拿铁","意式浓缩" };        //名称数组
-            double[] Prices = { 12.0, 15.5, 16.5, 16.5,10 };                 //价格数组
+ 
+            Product[] mune = new Product[]    //定义商品数组
+            { 
+                new Product("美食咖啡",12.0),
+                new Product("拿铁咖啡",15.5), 
+                new Product("香草拿铁",16.5),
+                new Product("抹茶拿铁",16.5),
+                new Product("意式浓缩",10.0)
+            };
+             //定义变量
+            int choise = 0;           //选择           
             string choiseName = " ";  //被选择的名称
             double choisePrise = 0;    //被选择商品价格
+
             double amount = 0;           //总计
             int num = 0;                //数量
             double pay = 0;            //单次价格
@@ -47,6 +47,7 @@ namespace X2402邹君诚_若雪咖啡
 
             while (true)
             {
+                displayMenu();  //输出菜单
                 Console.WriteLine("请输入您的选择：");
                 if (!int.TryParse(Console.ReadLine(), out choise) || choise <= 0 || choise > 6)
                 {
@@ -58,8 +59,8 @@ namespace X2402邹君诚_若雪咖啡
                     break;
                 }
 
-                choiseName = Names[choise - 1];
-                choisePrise = Prices[choise - 1];
+                choiseName = mune[choise - 1].Name;
+                choisePrise = mune[choise - 1].Price;
                 Console.WriteLine($"请问您需要几杯{choiseName}？"); 
                 if (!int.TryParse(Console.ReadLine(), out num)||num<0) 
                 {
@@ -92,5 +93,29 @@ namespace X2402邹君诚_若雪咖啡
             Console.ReadLine();
 
         }
+        static void displayWelcomeScreen()
+        {
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("-                                -");
+            Console.WriteLine("-        模拟自动售货机          -");
+            Console.WriteLine("-            若雪咖啡            -");
+            Console.WriteLine("-        NO.5 2024-12-26         -");
+            Console.WriteLine("-                                -");
+            Console.WriteLine("----------------------------------");
+        }
+        static void displayMenu()
+        {
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("-                                -");
+            Console.WriteLine("-            喜欢您来            -");
+            Console.WriteLine("-        1-美式咖啡     12.0元   -");
+            Console.WriteLine("-        2-拿铁咖啡     15.5元   -");
+            Console.WriteLine("-        3-香草拿铁     16.5元   -");
+            Console.WriteLine("-        4-抹茶拿铁     16.5元   -");
+            Console.WriteLine("-        5-意式浓缩     10.0元   -");
+            Console.WriteLine("-        6-退出点餐              -");
+            Console.WriteLine("-                                -");
+            Console.WriteLine("----------------------------------");
+        }
     }
-}
+}`
