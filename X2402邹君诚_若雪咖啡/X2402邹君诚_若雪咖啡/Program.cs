c4b21cbbf53b4f67e9a32211a76a5beb03ea70b2
Zou Juncheng <z20041219@outlook.com>
2024-12-27 22:24:48 +0800

    2024-12-27_22:24

--- X2402邹君诚_若雪咖啡/X2402邹君诚_若雪咖啡/Program.cs
+++ X2402邹君诚_若雪咖啡/X2402邹君诚_若雪咖啡/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace X2402邹君诚_若雪咖啡
@@ -15,17 +16,30 @@ namespace X2402邹君诚_若雪咖啡
             Price = price;
         }
     }
+    struct OrderItem  //定义结构体，点单项目
+    {
+        public string Name;
+        public int Quantity;
+        public double TotalPrice;
+        public OrderItem(string name, int quantity, double totalPrice)
+        {
+            Name = name;
+            Quantity = quantity;
+            TotalPrice = totalPrice;
+        }
+
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
             //界面
-            displayWelcomeScreen();  //使用方法输出界面
+            DisplayWelcomeScreen();  //使用方法输出界面
 
             //功能实现
 
  
-            Product[] mune = new Product[]    //定义商品数组
+            Product[] menu = new Product[]    //定义商品数组
             { 
                 new Product("美食咖啡",12.0),
                 new Product("拿铁咖啡",15.5), 
@@ -34,66 +48,64 @@ namespace X2402邹君诚_若雪咖啡
                 new Product("意式浓缩",10.0)
             };
              //定义变量
-            int choise = 0;           //选择           
-            string choiseName = " ";  //被选择的名称
-            double choisePrise = 0;    //被选择商品价格
-
+            int choice = 0;           //选择           
+            List<OrderItem> orders = new List<OrderItem>();
             double amount = 0;           //总计
             int num = 0;                //数量
-            double pay = 0;            //单次价格
-            double paid = 0;            //实付
-            double change=0;          //找零
+            double paid = 0;            //用户支付金额
+            double change=0;          //找零金额
             
 
             while (true)
             {
-                displayMenu();  //输出菜单
+                DisplayMenu();  //输出菜单
                 Console.WriteLine("请输入您的选择：");
-                if (!int.TryParse(Console.ReadLine(), out choise) || choise <= 0 || choise > 6)
+                if (!int.TryParse(Console.ReadLine(), out choice) || choice <= 0 || choice > 6)
                 {
                     Console.WriteLine("输入错误，请重新输入：");
                     continue;
                 }
-                else if (choise == 6)
+                if (choice == 6)
                 {
                     break;
                 }
 
-                choiseName = mune[choise - 1].Name;
-                choisePrise = mune[choise - 1].Price;
-                Console.WriteLine($"请问您需要几杯{choiseName}？"); 
+                string choiceName = menu[choice - 1].Name;    //局部变量，当前循环所点商品名称
+                double choicePrice = menu[choice - 1].Price;  //局部变量，当前循环所点商品单价
+                Console.WriteLine($"请问您需要几杯{choiceName}？"); 
                 if (!int.TryParse(Console.ReadLine(), out num)||num<0) 
                 {
                     Console.WriteLine("输入错误，请重新输入：");
+                    continue;
                 }      
-                pay = num * choisePrise;
+                double pay = num * choicePrice;              //局部变量，当前循环小计金额
                 amount += pay;
+                orders.Add(new OrderItem(choiceName,num,pay));
+                Console.WriteLine($"此次您点了{num}杯{choiceName},小计{pay}元");
             }
-            Console.WriteLine($"应付金额为{amount},请问您实付金额多少？");
-            while (!double.TryParse(Console.ReadLine(), out paid)||paid<=0)
+
+            Console.WriteLine($"应付金额{amount}元，请问您实付多少?");
+            paid=GetValidated();
+            while ((change = paid - amount) < 0)
             {
-                Console.WriteLine("输入错误，请重新输入：");
+                Console.WriteLine($"支付失败，您需要支付{amount}元");
+                Console.WriteLine("请支付：");
+                paid = GetValidated();
             }
-            change = paid - amount;
-            while (change < 0)
+
+            Console.WriteLine("您点的商品如下：");
+            foreach (var order in orders)
             {
-                double more = 0;
-                Console.WriteLine($"您付的钱还不够，您还需付{0-change}");
-                Console.WriteLine($"请支付：");
-                while (!double.TryParse(Console.ReadLine(), out more) ||more <= 0)
-                {
-                    Console.WriteLine("输入错误，请重新输入！");
-                }
-                change += more;
+                Console.WriteLine($"{order.Quantity}杯{order.Name}----共{order.TotalPrice}元");
             }
-            Console.WriteLine($"您一共点了{num}杯{choiseName}。\n账单为：\n应付 {amount}元\n实付 {paid}元\n找零 {change}元");
+            Console.WriteLine($"账单为：\n应付 {amount}元\n实付 {paid}元\n找零 {change}元");
             Console.WriteLine("喜欢您来，欢迎下次光临！");
 
             //暂停页面查看结果
             Console.ReadLine();
 
         }
-        static void displayWelcomeScreen()
+        static void DisplayWelcomeScreen()
         {
             Console.WriteLine("----------------------------------");
             Console.WriteLine("-                                -");
@@ -103,7 +115,7 @@ namespace X2402邹君诚_若雪咖啡
             Console.WriteLine("-                                -");
             Console.WriteLine("----------------------------------");
         }
-        static void displayMenu()
+        static void DisplayMenu()
         {
             Console.WriteLine("----------------------------------");
             Console.WriteLine("-                                -");
@@ -117,5 +129,21 @@ namespace X2402邹君诚_若雪咖啡
             Console.WriteLine("-                                -");
             Console.WriteLine("----------------------------------");
         }
+        static double GetValidated()
+        {
+            double value;
+            while (true) 
+            { 
+                if (double.TryParse(Console.ReadLine(),out value)&&value>=0)
+                {
+                    return value;
+
+                }
+                else
+                {
+                    Console.WriteLine("输入错误，请重新输入：");
+                }
+            }
+        }
     }
-}`
+}
